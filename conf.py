cce7f9295889fca48cfb0c874d16b52fba665b37
Olof hagsand <olof@hagsand.se>
2023-10-26 17:55:24 +0200

    Trying to get sphinx_rtd_theme

--- conf.py
+++ conf.py
@@ -40,7 +40,9 @@ highlight_language = 'sh'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+]
 #extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -89,7 +91,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'alabaster' # local compile
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 #html_theme = 'default' # ugly
 
 # Theme options are theme-specific and customize the look and feel of a theme
