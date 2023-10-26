bc108ab5bb5803ffcfb009a1340c9af30e9df491
Olof hagsand <olof@hagsand.se>
2023-10-26 15:41:45 +0200

    no sphinx theme

--- conf.py
+++ conf.py
@@ -89,7 +89,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'alabaster' # local compile
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 #html_theme = 'default' # ugly
 
 # Theme options are theme-specific and customize the look and feel of a theme
