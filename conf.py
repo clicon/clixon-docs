929eeca480811f148fe62db53c37a9b3f7941350
Olof hagsand <olof@hagsand.se>
2025-11-21 14:47:18 +0100

    Clicon 7.6

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '7.5'
+release = '7.6'
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
